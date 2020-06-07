a4eff47280489b1cdc9998908979a3cb3bce6504
hbajwaa <bajwahassan3@outlook.com>
2020-06-07 19:53:36 -0400

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_193603) do
+ActiveRecord::Schema.define(version: 2020_06_07_214749) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_193603) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "uid"
   end
 
   create_table "watchlists", force: :cascade do |t|
