5576dcf3f7a968f6315c30155bfa29c1a520c2c5
hbajwaa <bajwahassan3@outlook.com>
2020-06-07 19:49:25 -0400

    added facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
-    @user = User.find_by(email: params[:session][:email].downcase)
+
+    @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to user_watchlists_path(@user)
@@ -25,5 +25,22 @@ class SessionsController < ApplicationController
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  def create_by_facebook
+    @user = User.find_by(email: auth[:info][:email])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_watchlists_path(@user)
+    else
+      @user = User.create(name: auth[:info][:name], email: auth[:info][:email], uid: auth[:uid], password_digest: SecureRandom.hex)
+      session[:user_id] = @user.id
+      redirect_to user_watchlists_path(@user)
+    end
+  end
+
+  private
+  def auth
+    request.env['omniauth.auth']
   end
 end
