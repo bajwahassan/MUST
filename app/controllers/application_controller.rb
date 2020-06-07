7d065adc248fc4bf725e6f013335a102f6d4a01f
hbajwaa <bajwahassan3@outlook.com>
2020-06-07 00:36:17 -0400

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+
+  def current_user
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:danger] = "You must be logged in to access this section!"
+      redirect_to login_path
+    end
+  end
 end
